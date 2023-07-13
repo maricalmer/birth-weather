f98aa9375941f0d3efa6ac1b262b142da31cba5a
Mari Calmer <superlassig@gmail.com>
2023-07-13 23:42:54 +0200

    feat: reduce border radius on container

--- src/styled/cards/card.js
+++ src/styled/cards/card.js
@@ -4,22 +4,22 @@ import device from '../../utils/device_size_matcher'
 
 const Card = styled.div`
 	background-color: rgba(150, 167, 181, .5);
-	border-radius: 24px;
+	border-radius: 8px;
 	width: 100%;
 	height: 100%;
 	position: relative;
 	overflow: hidden;
 
-	@media ${device.mobileL} { 
+	@media ${device.mobileL} {
     width: 400px;
 		height: 75%;
   }
 
-	@media ${device.tablet} { 
+	@media ${device.tablet} {
     width: 500px;
   }
 
-	@media ${device.laptop} { 
+	@media ${device.laptop} {
     width: 650px;
   }
 `;
