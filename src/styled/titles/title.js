c32ad77c5e72705b92e93e3df7aad4e133549fb3
Mari Calmer <superlassig@gmail.com>
2023-08-05 19:32:21 +0200

    feat: emphasize birth date styling

--- src/styled/titles/title.js
+++ src/styled/titles/title.js
@@ -3,7 +3,8 @@ import device from '../../utils/device_size_matcher'
 
 const Title = styled.div`
 	font-family: 'DM Sans', sans-serif;
-	font-weight: 800;
+	font-weight: 600;
+  font-style: italic;
 	color: black;
 	text-align: center;
   z-index: 3;
@@ -14,6 +15,9 @@ const Title = styled.div`
 
 	@media ${device.laptop} {
 		margin-top: 8px;
+    font-style: normal;
+    margin-left: 1rem;
+    margin-right: 1rem;
   }
 `;
 
