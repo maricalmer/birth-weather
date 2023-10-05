ba27320571a37f15a095ac483fe9ddc23ee0f958
Mari Calmer <superlassig@gmail.com>
2023-10-05 13:52:33 +0200

    fix: update cloud gif because former is down

--- src/styled/cards/clouds.js
+++ src/styled/cards/clouds.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 const Clouds = styled.div`
 	width: 100%;
 	height: 100%;
-	background-image: url("https://i.giphy.com/media/RpwupnbQE5nK6iRkYJ/giphy.webp");
+	// background-image: url("https://i.giphy.com/media/RpwupnbQE5nK6iRkYJ/giphy.webp");
+	background-image: url("https://i.giphy.com/media/67uxmHhIF3uh6Ph8ew/giphy.webp");
 	background-repeat: no-repeat;
   background-size: cover;
 	border-top-left-radius: 8px;
