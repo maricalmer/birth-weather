e2f51ce372062f7148bccb7307e3838e690d3c1f
Mari Calmer <superlassig@gmail.com>
2023-10-05 14:17:06 +0200

    feat: set final option for gif replacement

--- src/styled/cards/clouds.js
+++ src/styled/cards/clouds.js
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 const Clouds = styled.div`
 	width: 100%;
 	height: 100%;
-	// background-image: url("https://i.giphy.com/media/67uxmHhIF3uh6Ph8ew/giphy.webp");
-	// background-image: url("https://i.giphy.com/media/ABMCiYH9MoVy0/giphy.webp");
-	// background-image: url("https://i.giphy.com/media/k3CeSrt9IZ6aorWCy1/giphy.webp");
 	background-image: url("https://i.giphy.com/media/fSXHTR5nSzKMOPVodt/giphy.webp");
 	background-repeat: no-repeat;
   background-size: cover;
