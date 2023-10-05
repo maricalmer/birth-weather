03c3194d99d06e3fe45123a816c4b0f94953aba2
Mari Calmer <superlassig@gmail.com>
2023-10-05 14:06:17 +0200

    feat: new clouds version for gif

--- src/styled/cards/clouds.js
+++ src/styled/cards/clouds.js
@@ -4,8 +4,9 @@ const Clouds = styled.div`
 	width: 100%;
 	height: 100%;
 	// background-image: url("https://i.giphy.com/media/67uxmHhIF3uh6Ph8ew/giphy.webp");
-	// background-image: url("https://i.giphy.com/media/ABMCiYH9MoVy0/200w.webp");
-	background-image: url("https://i.giphy.com/media/k3CeSrt9IZ6aorWCy1/200w.webp");
+	// background-image: url("https://i.giphy.com/media/ABMCiYH9MoVy0/giphy.webp");
+	// background-image: url("https://i.giphy.com/media/k3CeSrt9IZ6aorWCy1/giphy.webp");
+	background-image: url("https://i.giphy.com/media/fSXHTR5nSzKMOPVodt/giphy.webp");
 	background-repeat: no-repeat;
   background-size: cover;
 	border-top-left-radius: 8px;
